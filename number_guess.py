40909b8a70cbfaee2c494a3f9e50acd931c9cf5e
Sarang Gajbhare <139471163+SarangGajbhare@users.noreply.github.com>
2025-03-04 15:57:13 +0530

    Add files via upload
    Number guessing using Python Programming.

---
+++ number_guess.py
@@ -0,0 +1,28 @@
+import random
+
+while True:
+    
+    guess = random.randint(1,100)
+    attempt = 0
+
+    while True:
+        user_input = int(input("Enter a number between 1 to 100: "))
+        attempt+=1
+        if guess < user_input:
+            print("Number is to high")
+        elif guess > user_input:
+            print("Number is to low")
+        else:
+            print(f"Correct! You guessed im {attempt} attempt!")
+            break
+        if attempt == 3:
+            if guess % 2 == 0:
+                print("Hint: Number is Even")
+            else:
+                print(" Hint: Number is Odd")
+               
+    again = ("You want to play a game again!")
+    response = input("Enter a 'Yes' or 'No': ").lower()
+    if response != "yes":
+        print("Thanks For Playing a game!")
+        break
